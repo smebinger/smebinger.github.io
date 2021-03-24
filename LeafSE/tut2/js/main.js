5486482490a5ac4acebd5ffdcf291b03a5caf35c
Samara Ebinger <33261810+smebinger@users.noreply.github.com>
2021-03-23 21:18:27 -0400

    Update main.js

--- LeafSE/tut2/js/main.js
+++ LeafSE/tut2/js/main.js
@@ -3,6 +3,17 @@ var mymap = L.map('mapid').fitWorld();
 
 mymap.locate({setView: true, maxZoom: 14});
 
+function onLocationFound(e) {
+    var radius = e.accuracy;
+
+    L.marker(e.latlng).addTo(mymap)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+mymap.on('locationfound', onLocationFound);
+
 L.tileLayer('https://api.mapbox.com/styles/v1/smebinger/ckl5jwq954f5w17jysw36tsa0/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21lYmluZ2VyIiwiYSI6ImNqMGVlZ3ducjAwejEzMm5zOG05aWUzcjEifQ.BuETqSmrqIHckR2AdhGmFw', {
   attribution: 'Made with Leaflet and Mapbox.',
   maxZoom: 15,
