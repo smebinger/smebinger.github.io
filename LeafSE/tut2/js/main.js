5d758afe080e47f0ced02b3270c49ec959548a6b
Samara Ebinger <33261810+smebinger@users.noreply.github.com>
2021-03-23 21:24:59 -0400

    Update main.js

--- LeafSE/tut2/js/main.js
+++ LeafSE/tut2/js/main.js
@@ -9,10 +9,16 @@ function onLocationFound(e) {
     L.marker(e.latlng).addTo(mymap)
         .bindPopup("You are within " + radius + " meters from this point").openPopup();
 
-    L.circle(e.latlng, radius).addTo(map);
+    L.circle(e.latlng, radius).addTo(mymap);
 }
 
 mymap.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+mymap.on('locationerror', onLocationError);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/smebinger/ckl5jwq954f5w17jysw36tsa0/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic21lYmluZ2VyIiwiYSI6ImNqMGVlZ3ducjAwejEzMm5zOG05aWUzcjEifQ.BuETqSmrqIHckR2AdhGmFw', {
   attribution: 'Made with Leaflet and Mapbox.',
